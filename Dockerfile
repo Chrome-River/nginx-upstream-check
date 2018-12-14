76b5b96ffe29016939772cb5f85e99719cb95b17
Matt Noble <matthew.noble@chromeriver.com>
2018-12-14 15:33:33 -0800

    SO-8698 Update Nginx and configure errors to go to stderr

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ COPY ./install.sh /tmp/install.sh
 RUN /tmp/install.sh
 COPY ./nginx.conf /etc/nginx/nginx.conf
 
-ENV NGINX_VERSION 1.9.15
+ENV NGINX_VERSION 1.14.2
 
 RUN ln -sf /dev/stdout /var/log/nginx/access.log
 RUN ln -sf /dev/stderr /var/log/nginx/error.log
--- install.sh
+++ install.sh
@@ -3,15 +3,16 @@
 apt-get update
 apt-get install -y git gcc wget libpcre3 libpcre3-dev libssl-dev make build-essential zlib1g-dev libbz2-dev unzip
 
-#install nginx
+# Install Nginx with the Nginx upstream check module (https://github.com/yaoweibin/nginx_upstream_check_module) patched in
+#   The configuration parameter error-log-path is set to standard error to keep the error log out of the conatainer.
+#   Unlike the access log location, the error log location setting in the config file does not actually change the error log location
+#   See https://stackoverflow.com/questions/13371925/how-to-turn-off-or-specify-the-nginx-error-log-location
 cd /tmp
-#git clone https://github.com/SirAnthony/ngx_ustats_module.git
 git clone https://github.com/yaoweibin/nginx_upstream_check_module.git
-wget 'http://nginx.org/download/nginx-1.9.15.tar.gz'
-tar -xzvf nginx-1.9.15.tar.gz
-cd nginx-1.9.15
-patch -p0 < ../nginx_upstream_check_module/check_1.9.2+.patch
-#patch -p1 -i  ../ngx_ustats_module/nginx-1.9+.patch
+wget 'http://nginx.org/download/nginx-1.14.2.tar.gz'
+tar -xzvf nginx-1.14.2.tar.gz
+cd nginx-1.14.2
+patch -p0 < ../nginx_upstream_check_module/check_1.14.0+.patch
 ./configure --with-http_stub_status_module \
     --with-http_ssl_module \
     --with-http_realip_module \
@@ -28,7 +29,9 @@ patch -p0 < ../nginx_upstream_check_module/check_1.9.2+.patch
     --prefix=/usr \
     --conf-path=/etc/nginx/nginx.conf \
     --user=nginx \
-    --group=nginx 
+    --group=nginx \
+    --error-log-path=/dev/stderr
+
 make
 make install
 apt-get remove -y git gcc libssl-dev libpcre3-dev
