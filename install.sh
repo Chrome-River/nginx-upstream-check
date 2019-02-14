810ef897f73a5d94461208259229ec648fc07936
Viktor Kakhnych <vkakhnych@gmail.com>
2019-02-14 17:47:27 +0000

    Nginx version upgraded to 1.14.2, image cleanup, base config tuning

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y git gcc wget libpcre3 libpcre3-dev libssl-dev make build-essential zlib1g-dev libbz2-dev unzip
+apt update
+apt -y upgrade
+apt install -y git gcc wget libpcre3 libpcre3-dev libssl-dev make build-essential zlib1g-dev libbz2-dev unzip
 
 #install nginx
 cd /tmp
 #git clone https://github.com/SirAnthony/ngx_ustats_module.git
 git clone https://github.com/yaoweibin/nginx_upstream_check_module.git
-wget 'http://nginx.org/download/nginx-1.9.15.tar.gz'
-tar -xzvf nginx-1.9.15.tar.gz
-cd nginx-1.9.15
-patch -p0 < ../nginx_upstream_check_module/check_1.9.2+.patch
-#patch -p1 -i  ../ngx_ustats_module/nginx-1.9+.patch
+wget 'http://nginx.org/download/nginx-1.14.2.tar.gz'
+tar -xzvf nginx-1.14.2.tar.gz
+cd nginx-1.14.2
+patch -p1 < ../nginx_upstream_check_module/check_1.14.0+.patch
+#patch -p1 -i  ../ngx_ustats_module/nginx-1.14.0+.patch
 ./configure --with-http_stub_status_module \
     --with-http_ssl_module \
     --with-http_realip_module \
@@ -28,11 +29,12 @@ patch -p0 < ../nginx_upstream_check_module/check_1.9.2+.patch
     --prefix=/usr \
     --conf-path=/etc/nginx/nginx.conf \
     --user=nginx \
-    --group=nginx 
+    --group=nginx
 make
 make install
-apt-get remove -y git gcc libssl-dev libpcre3-dev
-apt-get clean
+apt remove -y git gcc libssl-dev libpcre3-dev
+apt -y autoremove
+apt clean
 
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 rm -fr /tmp/*
@@ -40,4 +42,5 @@ rm -fr /tmp/*
 mkdir /var/log/nginx
 mkdir /etc/nginx/conf.d
 rm -fr /etc/nginx/*.default
-adduser --system --no-create-home --shell /bin/false --group --disabled-login nginx
+#adduser --system --no-create-home --shell /bin/false --group --disabled-login nginx
+
